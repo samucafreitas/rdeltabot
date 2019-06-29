09d77dd179c29a8409d7cf25e489658785d10d56
samucaof@hotmail.com <samucaof@hotmail.com>
2019-06-29 16:53:56 -0300

    Added sofrer command handler

--- src/deltabot.py
+++ src/deltabot.py
@@ -1,38 +1,35 @@
 #!/usr/bin/env python3
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from guias import * # Importa todos os nossos guias
+from guias import *
 from db import init_db
 import logging
 import os
 
-# Habilita o log passando o formato do log
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
 def main():
-    # Informamos aqui nosso Token do bot
     updater = Updater(os.environ.get("BOT_TOKEN"))
 
-    # Dispatcher para registrar os nossos guias
     dp = updater.dispatcher
 
-    # Adiciona nossos guias aos respectivos comandos do bot
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("help", help_bot))
     dp.add_handler(CommandHandler("sentiment", sentiment))
-    # TODO: hora de ir pra facul
-    #dp.add_handler(CommandHandler("", sofrer))
 
-    # Adiciona o guia de erros
+    # TODO: hora de ir pra facul
+    dp.add_handler(CommandHandler("hoje", sofrer))
+
     dp.add_error_handler(error)
 
-    # Inicia o bot
     updater.start_polling()
     updater.idle()
 
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
 
 if __name__ == '__main__':
     init_db()
--- src/guias.py
+++ src/guias.py
@@ -1,12 +1,9 @@
-# Um guia é uma função que guia nosso bot quando ele recebe algum comando ou msg
 import sentiment_analysis
 
-# Guia para quando recebemos o comando /start no privado
 def start(bot, update):
     update.message.reply_text('Olá, seja bem vindo!\nPara pedir ajuda: /help')
 
-# Nosso guia de help para os nossos comandos
-def help(bot, update):
+def help_bot(bot, update):
     update.message.reply_text('Help!')
 
 def sentiment(bot, update):
@@ -14,11 +11,5 @@ def sentiment(bot, update):
     prediction = sentiment_analysis.predict(msg)
     update.message.reply_text(prediction)
 
-# Esse guia veio junto ao exemplo da interface para o Telegram Bot API
-# Não usaremos!
-# def echo(bot, update):
-#    update.message.reply_text(update.message.text)
-
-# Aqui é o guia dos erros 
-def error(bot, update, error):
-    logger.warning('Update "%s" caused error "%s"', update, error)
+def sofrer(bot, update):
+    update.message.reply_text('Aula no lab 17')
