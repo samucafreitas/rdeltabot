849b2853a5bd737b75026efc8afc53b5f86b8d03
samucaof@hotmail.com <samucaof@hotmail.com>
2019-07-06 09:51:43 -0300

    Removed spaces

--- src/deltabot.py
+++ src/deltabot.py
@@ -21,7 +21,6 @@ def main():
     dp.add_handler(CommandHandler("hoje", sofrer))
     dp.add_handler(MessageHandler(Filters.group, lucianna))
 
-
     dp.add_error_handler(error)
 
     updater.start_polling()
