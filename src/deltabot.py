a46cd51429a63e4097645378bb13eb6e0825d28c
samucaof@hotmail.com <samucaof@hotmail.com>
2019-07-06 09:10:35 -0300

    Added filter.group

--- src/deltabot.py
+++ src/deltabot.py
@@ -19,7 +19,7 @@ def main():
     dp.add_handler(CommandHandler("help", help_bot))
     dp.add_handler(CommandHandler("sentiment", sentiment))
     dp.add_handler(CommandHandler("hoje", sofrer))
-    dp.add_handler(MessageHandler(Filters.text, lucianna))
+    dp.add_handler(MessageHandler(Filters.group, lucianna))
 
 
     dp.add_error_handler(error)
